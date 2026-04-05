d04de4d0ea0ace149349f2de5c3b8ebb3a9db5f3
Rafael Brandao <rbs13bg13@gmail.com>
2026-04-05 23:55:57 +0000

    adicionado validação de usuario

--- src/main/java/com/brandon/finance/user/service/UserService.java
+++ src/main/java/com/brandon/finance/user/service/UserService.java
@@ -3,7 +3,9 @@ package com.brandon.finance.user.service;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.brandon.finance.helper.SecurityUtils;
 import com.brandon.finance.shared.base.excepetion.ResourceNotFoundException;
+import com.brandon.finance.shared.base.excepetion.UnauthorizedException;
 import com.brandon.finance.user.mapper.UserMapper;
 import com.brandon.finance.user.repository.UserRepository;
 import com.brandon.finance.user.request.CreateUserRequest;
@@ -30,6 +32,12 @@ public class UserService {
     public UserResponse getUserById(Long id) {
         var user = userRepository.findById(id)
             .orElseThrow(() -> new ResourceNotFoundException("Usuario não encontrado"));
+
+        var userLoggin = SecurityUtils.getAuthenticatedUser();
+
+        if(!userLoggin.getId().equals(id)) {
+            throw new UnauthorizedException("Você não pode acessar este recurso");
+        }
         return userMapper.toResponse(user);
     }
     
