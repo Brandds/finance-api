d330d62e361a25487c2592ac59db3cbf5da094ea
Rafael Brandao <rbs13bg13@gmail.com>
2026-08-18 01:20:07 +0000

    feat (categoriyEnum): cria enum para icon de categoria

---
+++ src/main/java/com/brandon/finance/category/enums/CategoryIconEnum.java
@@ -0,0 +1,12 @@
+package com.brandon.finance.category.enums;
+
+public enum CategoryIconEnum {
+    FOOD,
+    TRANSPORT,
+    LEISURE,
+    HEALTH,
+    HOME,
+    EDUCATION,
+    SHOPPING,
+    BILLS,
+}
